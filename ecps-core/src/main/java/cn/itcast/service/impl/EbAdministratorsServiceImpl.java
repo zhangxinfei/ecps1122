e1b3bc57329071c04eb7f476755f138a8a46fe9c
zhangxinfei <zhangxinfei@qq.com>
2018-08-25 10:36:18 +0800

    [M]修改jdk

--- ecps-core/src/main/java/cn/itcast/service/impl/EbAdministratorsServiceImpl.java
+++ ecps-core/src/main/java/cn/itcast/service/impl/EbAdministratorsServiceImpl.java
@@ -1,12 +1,13 @@
 package cn.itcast.service.impl;
 
-import cn.itcast.dao.EbAdministratorsDao;
 import cn.itcast.model.Administrators;
+import cn.itcast.model.User;
+import cn.itcast.service.EbAdministratorsService;
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 
 import java.util.List;
 
-public class EbAdministratorsServiceImpl extends SqlSessionDaoSupport implements EbAdministratorsDao {
+public class EbAdministratorsServiceImpl implements EbAdministratorsService {
 
     public List<Administrators> selectByPrimaryKey(int administrators_id) throws Exception {
         return null;
@@ -16,7 +17,22 @@ public class EbAdministratorsServiceImpl extends SqlSessionDaoSupport implements
 
     }
 
+    @Override
+    public List<User> selectUserById(int user_id) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void insert(User user) throws Exception {
+
+    }
+
     public void deleteByPrimaryKey(int administrators_id) throws Exception {
+
+    }
+
+    @Override
+    public void updateByPrimaryKey(User user) throws Exception {
 
     }
 
