6cfdd809bc15c62d0dfd9a88d537f66adc12d163
zhangxinfei <zhangxinfei@qq.com>
2018-09-03 08:10:13 +0800

    [M]修改jdk版本到1.8

--- ecps-core/src/main/java/cn/itcast/service/Impl/UserServiceImpl.java
+++ ecps-core/src/main/java/cn/itcast/service/Impl/UserServiceImpl.java
@@ -28,11 +28,13 @@ public class UserServiceImpl implements UserService {
 //        初始化序列化结果集
         ResultModel result = ResultUtil.info(WizardAuditEnum.StatusEnum.STATUS_FAIL.getValue(),WizardAuditEnum.StatusEnum.STATUS_FAIL.getDesc());
 //        查询数据库，返回值为0的化就是没有登录失败，为1的化就是成功
+        logger.info("登录验证—>start");
         int influence = userMapper.selectByKeyAndPassword(user);
         if(influence == 1){
             result = ResultUtil.success(WizardAuditEnum.StatusEnum.STATUS_SUCCESS.getValue(),WizardAuditEnum.StatusEnum.STATUS_SUCCESS.getDesc());
-            logger.info("登录成功->success");
+            logger.info("登录成功->end");
         }
+            logger.info("登录失败—>end");
         return result;
     }
 }
